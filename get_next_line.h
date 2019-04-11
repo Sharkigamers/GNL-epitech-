4c4d0c587c440e0672c877fcbe8110b1bdc67e36
Sharkigamers <sharkigamers@localhost.localdomain>
2019-04-11 07:08:41 -0400

    [Add]: Project gnl_epitech for my_github

---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2018
+** get_next_line
+** File description:
+** .h for the project
+*/
+
+#ifndef READ_SIZE
+#define READ_SIZE 10000
+#endif
+
+char *get_next_line(int fd);
